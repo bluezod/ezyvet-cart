308c8d4661a90a34189aac556045d679a0fbdf84
Skyler Lan <bluezod@gmail.com>
2018-12-11 23:40:22 +1300

    Complete most functions

--- Cart.php
+++ Cart.php
@@ -8,7 +8,12 @@
 
 class Cart
 {
-    protected $items = [];
+    protected $items = array();
+
+    public function __construct(array $items = array())
+    {
+        $this->items = $items;
+    }
 
     public function getItems()
     {
@@ -17,27 +22,45 @@ class Cart
 
     public function add($name, $price)
     {
-
+        $qty = 1;
+        try {
+            if (isset($this->items[$name])) {
+                $qty += $this->items[$name]['qty'];
+            }
+            $rowTotal = number_format(round($price * $qty, 2), 2);
+            $this->items[$name] = array(
+                'name' => $name,
+                'unit_price' => $price,
+                'qty' => $qty,
+                'row_total' => $rowTotal,
+            );
+        } catch (Exception $ex) {
+            return false;
+        }
+        return true;
     }
 
     public function remove($name)
     {
-
-    }
-
-    public function getItemQty($name)
-    {
-
-    }
-
-    public function getItemTotal($name)
-    {
-
+        try {
+            if (isset($this->items[$name])) {
+                unset($this->items[$name]);
+            } else {
+                return false;
+            }
+        } catch (Exception $ex) {
+            return false;
+        }
+        return true;
     }
 
     public function getOverallTotal()
     {
-
+        $grandTotal = 0;
+        foreach ($this->items as $item) {
+            $grandTotal += $item['row_total'];
+        }
+        return number_format(round($grandTotal, 2), 2);
     }
 
     public function isEmpty()
--- index.php
+++ index.php
@@ -16,24 +16,40 @@ $products = array(
     array("name" => "Hacksaw", "price" => 18.45)
 );
 // ##################################################
-// Retrieve cart object stored in session
+$success = false;
+// Retrieve cart items array stored in session
 if (isset($_SESSION['cart'])) {
-    $cart = $_SESSION['cart'];
+    $cartItems = $_SESSION['cart'];
 } else {
-    // Init the cart object
-    $cart = new Cart;
+    $cartItems = array();
 }
-if (isset($_GET['cart_action'])) {
+// Init the cart object
+$cart = new Cart($cartItems);
+if (isset($_GET['cart_action']) && isset($_GET['product_name'])) {
     $cartAction = $_GET['cart_action'];
+    $productName = $_GET['product_name'];
     switch ($cartAction) {
         case 'add':
+            $unitPrice = 0;
+            foreach ($products as $product) {
+                if ($product['name'] == $productName) {
+                    $unitPrice = $product['price'];
+                    break;
+                }
+            }
+            if ($unitPrice) {
+                $success = $cart->add($productName, $unitPrice);
+            }
             break;
         case  'remove':
+            $success = $cart->remove($productName);
             break;
         default:
             break;
     }
-    $_SESSION['cart'] = $cart;
+    if ($success) {
+        $_SESSION['cart'] = $cart->getItems();
+    }
 }
 ?>
 
@@ -64,16 +80,27 @@ if (isset($_GET['cart_action'])) {
 <?php if ($cart->isEmpty()) : ?>
     Your cart is empty. Please feed it with some products.
 <?php else: ?>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+            <th>Action</th>
+        </tr>
+        <?php foreach ($cart->getItems() as $item): ?>
+            <form method="post" action="index.php?cart_action=remove&product_name=<?php echo $item['name'] ?>">
+                <tr>
+                    <td><?php echo isset($item['name']) ? $item['name'] : '' ?></td>
+                    <td><?php echo isset($item['unit_price']) ? number_format($item['unit_price'], 2) : '' ?></td>
+                    <td><?php echo isset($item['qty']) ? $item['qty'] : '' ?></td>
+                    <td><?php echo isset($item['row_total']) ? number_format($item['row_total'], 2) : '' ?></td>
+                    <td><input type="submit" value="Remove"/></td>
+                </tr>
+            </form>
+        <?php endforeach; ?>
+    </table>
 <?php endif; ?>
 <hr>
 <h1>Overall Total</h1>
-<?php
-$totalAmount = 0;
-if (!$cart->isEmpty()) {
-    foreach ($cart->getItems() as $item) {
-        $totalAmount += isset($item['row_total']) ? $item['row_total'] : 0;
-    }
-}
-?>
-The total amount of your cart is <strong><?php echo $totalAmount ?></strong>.
-<?php var_dump($products); ?>
+The total amount of your cart is <strong><?php echo $cart->getOverallTotal() ?></strong>.
